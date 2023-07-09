3394a3fbfe789874abe69fdc68caf9ca6e5fd7ac
Dev Bilaspure <devbilaspure4941@gmail.com>
2023-07-10 02:14:56 +0530

    minor fix

--- src/middlewares/authMiddlewares.ts
+++ src/middlewares/authMiddlewares.ts
@@ -29,35 +29,37 @@ export const authenticateToken = (
       });
       return;
     }
-    req.body.authorizedUser = user;
     next();
   });
 };
 
-// export const authenticateToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-//   if (!token) {
-//     res.status(401).json({
-//       success: false,
-//       message: "Token not found",
-//       errorType: TOKEN_NOT_FOUND,
-//     });
-//     return;
-//   }
-//   jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
-//     if (err) {
-//       res.clearCookie("token").status(403).json({
-//         success: false,
-//         message: "Invalid token",
-//         errorType: INVALID_TOKEN,
-//       });
-//       return;
-//     }
-//     next();
-//   });
-// };
+
+
+export const authenticateTokenInitializeUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    res.status(401).json({
+      success: false,
+      message: "Token not found",
+      errorType: TOKEN_NOT_FOUND,
+    });
+    return;
+  }
+  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
+    if (err) {
+      res.clearCookie("token").status(403).json({
+        success: false,
+        message: "Invalid token",
+        errorType: INVALID_TOKEN,
+      });
+      return;
+    }
+    req.body.authorizedUser = user;
+    next();
+  });
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -6,14 +6,14 @@ import {
   logoutUser,
   sayHiToUser,
 } from "../controllers/authController";
-import { authenticateToken } from "../middlewares/authMiddlewares";
+import { authenticateTokenInitializeUser } from "../middlewares/authMiddlewares";
 
 const router = Router();
 
 router.get("/sayhi", sayHiToUser);
 router.post("/signup", createUser);
 router.post("/login", loginUser);
-router.get("/me", authenticateToken, getMe);
+router.get("/me", authenticateTokenInitializeUser, getMe);
 router.get("/logout", logoutUser)
 
 export default router;
